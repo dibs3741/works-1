8904f411a9df5d440a95128888c9f206fd49239a
Dibyendu Majumder <d_majumder@yahoo.com>
2020-04-10 17:51:40 -0400

    fixed a problem with repeated duplicate elements

--- src/llist_1.py
+++ src/llist_1.py
@@ -4,7 +4,14 @@ class cNode:
         self.data = data
         self.next = None
 
-lstElements = ['a','b','c','a','x']
+def print_ll(llst_holder):
+    llst_pointer = llst_holder
+    while llst_pointer is not None:
+        print(llst_pointer.data)
+        llst_pointer = llst_pointer.next
+
+
+lstElements = ['a','b','c','a','a','x']
 
 llst_holder = cNode(lstElements[0])
 llst_pointer = llst_holder
@@ -20,11 +27,8 @@ llst_pointer_prev = None
 while llst_pointer is not None:
     if llst_pointer.data in visited:
         llst_pointer_prev.next = llst_pointer.next
-    visited.append(llst_pointer.data)
-    llst_pointer_prev = llst_pointer
+    else:
+        visited.append(llst_pointer.data)
+        llst_pointer_prev = llst_pointer
     llst_pointer = llst_pointer.next
-
-llst_pointer = llst_holder
-while llst_pointer is not None:
-    print(llst_pointer.data)
-    llst_pointer = llst_pointer.next
+print_ll(llst_holder)
