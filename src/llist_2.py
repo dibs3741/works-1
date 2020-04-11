e544a0198e6ee1cfb6902363f8f7bfca5c17ab52
Dibyendu Majumder <d_majumder@yahoo.com>
2020-04-10 21:04:09 -0400

    new file: find kth elem in linked list in reverse

--- src/llist_2.py
+++ src/llist_2.py
@@ -4,31 +4,34 @@ class cNode:
         self.data = data
         self.next = None
 
-def print_ll(llst_holder):
-    llst_pointer = llst_holder
-    while llst_pointer is not None:
-        print(llst_pointer.data)
-        llst_pointer = llst_pointer.next
 
+def print_ll(llst_holder1):
+    ll = llst_holder1
+    while ll is not None:
+        print(ll.data)
+        ll = ll.next
+
+
+def create_linklist_from_list(lst):
+    llst_holder = cNode(lst[0])
+    llst_pointer = llst_holder
+    for i in lst[1:]:
+        o = cNode(i)
+        llst_pointer.next = o
+        llst_pointer = o
+    return llst_holder
+
+
+def find_elem_at_pos(linklist, pos):
+    if linklist is None:
+        return 0, None
+    i, val = find_elem_at_pos(linklist.next, pos)
+    if i == pos:
+        return i+1, linklist.data
+    return i+1, val
 
 lstElements = ['a','b','c','a','a','x']
+llst_holder = create_linklist_from_list(lstElements)
+i, val = find_elem_at_pos(llst_holder, 4)
+print (val)
 
-llst_holder = cNode(lstElements[0])
-llst_pointer = llst_holder
-for i in lstElements[1:]:
-    o = cNode(i)
-    llst_pointer.next = o
-    llst_pointer = o
-
-visited = []
-llst_pointer = llst_holder
-llst_pointer_prev = None
-
-while llst_pointer is not None:
-    if llst_pointer.data in visited:
-        llst_pointer_prev.next = llst_pointer.next
-    else:
-        visited.append(llst_pointer.data)
-        llst_pointer_prev = llst_pointer
-    llst_pointer = llst_pointer.next
-print_ll(llst_holder)
